ab8e42ec0828bc7fc529edf13a95b54b8a85ff35
Alessandro Sfratta <alessandrosfrattadf@gmail.com>
2024-06-11 22:27:27 +0200

    menu logic 3/4

--- menu-navigation.js
+++ menu-navigation.js
@@ -2,6 +2,7 @@ import { togglePage } from "./form-wizard.js";
 
 
 const navBarItem = document.querySelectorAll('.navbar-item');
+const contNavbarItem = document.querySelectorAll('.cont-navbar-item')
 
 navBarItem.forEach( function (section) {
 
@@ -10,6 +11,30 @@ navBarItem.forEach( function (section) {
      
         togglePage(this.id,contSee);
 
+        contNavbarItem.forEach( function (navbarItem) {
+        
+        const contWithClass = navbar.closest(navbarItem).classList
+
+        contWithClass.contains("activeBtn") ? 
+        contWithClass.remove("activeBtn") : contWithClass.add("activeBtn")
+
+        } )
+
     });
+
+})
+
+
+const navbarMenuCont = document.querySelector('.navbar');
+const btnToggle = document.querySelector('.button-menu');
+const spanIcon = btnToggle.querySelector('span');
+
+btnToggle.addEventListener('click', function () {
+
+    navbarMenuCont.classList.contains('toggle') ? 
+    navbarMenuCont.classList.remove('toggle') : navbarMenuCont.classList.add('toggle');
+
+    spanIcon.classList.contains('rotate') ? 
+    spanIcon.classList.remove('rotate') : spanIcon.classList.add('rotate')
 
 })
